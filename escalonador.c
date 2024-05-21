a7967767bb009f8093d1a0da76c3c6c23f2ab7d8
Priscila Silva <priscilaangell123@gmail.com>
2024-05-20 21:56:23 -0300

    Tenta implementar comunicação por fila de mensagem

--- escalonador.c
+++ escalonador.c
@@ -9,19 +9,47 @@ int main(int argc, char **argv){
 
     int cores = atoi(argv[1]);
     printf("Cores: %d\n", cores);
+
+    int pidTeste15, pidTeste30 = 0;
+
+    struct processo
+    {
+        int pid;
+        int index;
+        float time;
+    };
+    
+    struct processo env;
+
+
+    int queue = msgget(0x1900, IPC_CREAT | 0x1FF);
+    printf("Queue: %x\n", queue);
+
     int pid = fork();
 
-    if(pid == 0){
-        int ret = execl("./teste15", "teste15", NULL);
-        printf("%d", ret);
-    }
+    
 
-    pid = fork();
+    // msgctl(queue, IPC_RMID, NULL);
 
     if(pid == 0){
-        int ret = execl("./teste30", "teste30", NULL);
-        printf("%d", ret);
+        int ret = execl("./teste15", "teste15", queue, 0, NULL);
+        printf("ret teste15");
+    } else {
+        pidTeste15 = pid;
+
+        pid = fork();
+
+        if(pid == 0){
+            int ret = execl("./teste30", "teste30", NULL);
+            printf("%d", ret);
+        } else {
+            pidTeste30 = pid;
+
+            printf("%d\n", pidTeste15);
+            printf("%d\n", pidTeste30);
+        }
     }
+
     return 0;
 
 }
